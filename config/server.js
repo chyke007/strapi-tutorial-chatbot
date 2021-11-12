dbf93858ce827b86744fef7abd93b789b9641e59
chibuike <chibuike@spleet.ng>
2021-11-12 14:59:00 +0100

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '375afa74fca850ae344484dfac036093'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6500fd75-fa07-4b23-ac1c-5c95535b8de4'
+};
